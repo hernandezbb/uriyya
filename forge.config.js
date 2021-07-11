3cae5948918fac008c72ad294265dfd90d8c9a1c
hernandezbb <bhbarquero@outlook.com>
2021-07-10 23:57:41 -0600

    Update node and configuration for build

--- forge.config.js
+++ forge.config.js
@@ -1,21 +1,37 @@
 module.exports = {
   packagerConfig: {},
+  makers: [
+    {
+      name: '@electron-forge/maker-squirrel',
+      config: {
+        name: 'uriyya',
+      },
+    },
+    {
+      name: '@electron-forge/maker-zip',
+      platforms: ['darwin'],
+    },
+    {
+      name: '@electron-forge/maker-deb',
+      config: {},
+    },
+  ],
   plugins: [
     [
-      "@electron-forge/plugin-webpack",
+      '@electron-forge/plugin-webpack',
       {
-        mainConfig: "./webpack.main.config.js",
+        mainConfig: './webpack.main.config.js',
         renderer: {
-          config: "./webpack.renderer.config.js",
+          config: './webpack.renderer.config.js',
           entryPoints: [
             {
-              html: "./src/renderer-process/index.html",
-              js: "./src/renderer-process/index.ts",
-              name: "main_window"
-            }
-          ]
-        }
-      }
-    ]
-  ]
+              html: './src/renderer-process/index.html',
+              js: './src/renderer-process/index.ts',
+              name: 'main_window',
+            },
+          ],
+        },
+      },
+    ],
+  ],
 };
